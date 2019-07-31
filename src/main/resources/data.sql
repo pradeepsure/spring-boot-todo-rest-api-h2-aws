575af05975c200c598080a3ec46100b4bc20c99f
pradeepsure <svnspradeepkumar@gmail.com>
2019-07-31 17:24:26 +0530

    enjoy

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,4 +8,4 @@ insert into todo(id, username,description,target_date,is_done)
 values(10003, 'in28minutes', 'Learn Microservices', sysdate(), false);
 
 insert into todo(id, username,description,target_date,is_done)
-values(10004, 'in28minutes', 'Learn AWS', sysdate(), false);
+values(10004, 'in28minutes', 'Learn AWS', sysdate(), true);
