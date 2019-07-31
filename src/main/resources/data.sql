b076df28983b8fb8e826da5a36424affd9848436
Sure <746983@cognizant.com>
2019-07-31 10:20:26 +0530

    dataFileChanged

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,6 @@ values(10002, 'in28minutes', 'Learn Data JPA', sysdate(), false);
 
 insert into todo(id, username,description,target_date,is_done)
 values(10003, 'in28minutes', 'Learn Microservices', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10004, 'in28minutes', 'Learn AWS', sysdate(), false);
