8ae512eb01ae3e33e9360ed1638471632e277448
pradeepsure <svnspradeepkumar@gmail.com>
2019-08-02 15:20:13 +0530

    finalCommit

--- src/main/java/org/pradeep/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
+++ src/main/java/org/pradeep/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -1,4 +1,4 @@
-package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+package org.pradeep.rest.webservices.restfulwebservices.helloworld;
 
 public class HelloWorldBean {
 
--- src/main/java/org/pradeep/rest/webservices/restfulwebservices/todo/TodoJpaRepository.java
+++ src/main/java/org/pradeep/rest/webservices/restfulwebservices/todo/TodoJpaRepository.java
@@ -1,4 +1,4 @@
-package com.in28minutes.rest.webservices.restfulwebservices.todo;
+package org.pradeep.rest.webservices.restfulwebservices.todo;
 
 import java.util.List;
 
