b8c239f9bea773c4bc998005031094c6c6d514d1
pradeepsure <svnspradeepkumar@gmail.com>
2019-08-02 13:01:00 +0530

    againChangedBean

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -15,7 +15,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World!!! this is from bean");
+		return new HelloWorldBean("Hello World!!! this is from bean done by pradeep");
 	}
 	
 	///hello-world/path-variable/in28minutes
