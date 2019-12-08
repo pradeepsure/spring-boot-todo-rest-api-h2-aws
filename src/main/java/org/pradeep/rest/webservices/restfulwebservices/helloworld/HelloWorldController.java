55ded0d71858ba51af256e0a2726ce0bd9fe87f9
pradeepsure <svnspradeepkumar@gmail.com>
2019-12-08 15:03:56 +0530

    MyThinkpadCommit1

--- src/main/java/org/pradeep/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/org/pradeep/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -15,7 +15,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World!!! this is from bean done by pradeep and seen karunakaran");
+		return new HelloWorldBean("Hello World!!! this is from bean done by pradeep and seen svns");
 	}
 	
 	///hello-world/path-variable/in28minutes
