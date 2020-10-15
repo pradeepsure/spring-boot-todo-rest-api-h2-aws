d1ebe159ec2d057aeea29b67197e5fa570a87917
pradeepsure <svnspradeepkumar@gmail.com>
2020-10-15 13:48:45 +0530

    commiting in intelliji editor

--- src/main/java/org/pradeep/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/org/pradeep/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -15,7 +15,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World!!! this is from bean done by pradeep and seen svns");
+		return new HelloWorldBean("Hello World!!! this is from bean done by pradeep, changed in intellij editor");
 	}
 	
 	///hello-world/path-variable/in28minutes
